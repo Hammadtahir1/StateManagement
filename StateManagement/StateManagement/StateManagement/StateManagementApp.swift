36078f1c38534509a4e14e0783feb9a3697f9202
Hammadtahir1 <156454748+Hammadtahir1@users.noreply.github.com>
2024-01-18 11:12:49 +0500

    Add files via upload
    initial commit

---
+++ StateManagement/StateManagement/StateManagement/StateManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateManagementApp.swift
+//  StateManagement
+//
+//  Created by Hamza on 18/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct StateManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
